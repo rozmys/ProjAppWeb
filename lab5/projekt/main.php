a5063d9ab728cd657875d2b62550a2418d09d525
rozmys <rozmys29@gmail.com>
2023-11-15 15:15:10 +0100

    Lab 5

---
+++ lab5/projekt/main.php
@@ -0,0 +1,31 @@
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Language" content="pl" />
+    <meta name="Author" content="Krzysztof Rozmysłowicz" />
+    <title>TOP 3 Samochody na Nurburgring</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/kolorujto.js" type="text/javascript"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+    <div id="menu">
+        <table>
+            <tr>
+                <td><a href="index.html"><b>Nurburgring</b></a></td>
+                <td><a href="/html/merc.html"><b>#1 Mercedes-AMG One</b></a></td>
+                <td><a href="/html/porsch.html"><b>#2 Porsche 911 GT2 RS MR</b></a></td>
+                <td><a href="/html/merc2.html"><b>#3 Mercedes-AMG Black Series</b></a></td>
+                <td><a href="/html/kontakt.html"><b>Kontakt</b></a></td>
+                <td><a href="/html/kolory.html"><b>Skrypt</b></a></td>
+            </tr>
+        </table>
+    </div>
+    <div id="content"></div>
+    <div id="footer">
+    <?php
+ $nr_indeksu = "1234567";
+ $nrGrupy = "X";
+ echo "<br /><br />Autor: Krzysztof Rozmysłowicz ".$nr_indeksu." grupa ".$nrGrupy;
+?>
+    </div>
+</body>
